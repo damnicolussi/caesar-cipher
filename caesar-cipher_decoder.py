40068cdc5b9ccc8d1c2b5d8b5a8da098b8ca3c2a
Damiano Nicolussi <68129014+damnicolussi@users.noreply.github.com>
2021-04-02 14:33:04 +0200

    Add files via upload

---
+++ caesar-cipher_decoder.py
@@ -0,0 +1,13 @@
+# rot13 decoder in python - github.com/
+
+def rot13(phrase, rot):
+   abc = "abcdefghijklmnopqrstuvwxyz"
+   out_phrase = ""
+   for char in phrase:
+       out_phrase += abc[(abc.find(char)+(26-rot))%26]
+   return out_phrase
+
+phrase = input('Enter your phrase: ')
+rot = int(input('Enter the rotation: '))
+
+print(rot13(phrase, rot))
---
+++ caesar-cipher_encoder.py
@@ -0,0 +1,13 @@
+# rot13 encoder in python - github.com/
+
+def rot13(phrase, rot):
+   abc = "abcdefghijklmnopqrstuvwxyz"
+   out_phrase = ""
+   for char in phrase:
+       out_phrase += abc[(abc.find(char)+rot)%26]
+   return out_phrase
+
+phrase = input('Enter your phrase: ')
+rot = int(input('Enter the rotation: '))
+
+print(rot13(phrase, rot))
